dfef86a2dca590dfa0ebb45fa140ec7e64cf4248
Ilkin Masimli <ilkin.masimli98@gmail.com>
2023-01-24 21:52:32 -0800

    first commit

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Animal
+    {
+        public string name;
+        public int age;
+        public string breed;
+        public string color;
+
+    public string GetDetails()
+        {
+            return $"{name} {age} {breed} ";
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal1 = new Animal();
+            animal1.age = 1;
+            animal1.breed = "dog";
+            animal1.name = "Jake";
+            animal1.color = "white";
+            Console.WriteLine(animal1.GetDetails());
+
+            var animal2 = new Animal();
+            animal2.age = 3;
+            animal2.breed = "cat";
+            animal2.name = "Tom";
+            animal2.color = "black";
+            Console.WriteLine(animal2.GetDetails());
+        }
+    }
+}
